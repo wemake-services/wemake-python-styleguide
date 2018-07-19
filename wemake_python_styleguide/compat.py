91eced8a2ad1b03a110bd15687c13bf935a71949
sobolevn <mail@sobolevn.me>
2018-07-19 14:40:13 +0300

    Adds fix to compat style

--- wemake_python_styleguide/compat.py
+++ wemake_python_styleguide/compat.py
@@ -13,7 +13,7 @@ def maybe_set_parent(tree: ast.AST) -> ast.AST:
     """Sets parents for all nodes that do not have this prop."""
     for statement in ast.walk(tree):
         for child in ast.iter_child_nodes(statement):
-            if not hasattr(child, 'parent'):  # noqa: Z105
-                setattr(child, 'parent', statement)  # noqa: Z105
+            if not hasattr(child, 'parent'):  # noqa: Z113
+                setattr(child, 'parent', statement)  # noqa: Z113
 
     return tree
