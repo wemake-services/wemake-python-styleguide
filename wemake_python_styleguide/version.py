180a172515a2d7cb4a4fd48f879004b0d976733b
sobolevn <mail@sobolevn.me>
2018-10-01 15:58:03 +0300

    Fixes coverage

--- wemake_python_styleguide/version.py
+++ wemake_python_styleguide/version.py
@@ -2,16 +2,16 @@
 
 import pkg_resources
 
-pkg_name = 'wemake-python-styleguide'
 
-
-def _get_version(dist_name: str) -> str:
+def _get_version(dist_name: str) -> str:  # pragma: no cover
     """Fetches distribution name. Contains a fix for Sphinx."""
     try:
         return pkg_resources.get_distribution(dist_name).version
     except pkg_resources.DistributionNotFound:
-        return '0.0.0'
+        return ''  # readthedocs can not install `poetry` projects
 
+
+pkg_name = 'wemake-python-styleguide'
 
 #: We store the version number inside the `pyproject.toml`:
 pkg_version: str = _get_version(pkg_name)
