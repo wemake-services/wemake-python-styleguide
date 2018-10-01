b52936cd75adaedde68b6eaad12773e7551003ba
sobolevn <mail@sobolevn.me>
2018-10-01 14:51:17 +0300

    Fixes checker docs

--- wemake_python_styleguide/version.py
+++ wemake_python_styleguide/version.py
@@ -4,5 +4,14 @@ import pkg_resources
 
 pkg_name = 'wemake-python-styleguide'
 
+
+def _get_version(dist_name: str) -> str:
+    """Fetches distribution name. Contains a fix for Sphinx."""
+    try:
+        return pkg_resources.get_distribution(dist_name).version
+    except pkg_resources.DistributionNotFound:
+        return '0.0.0'
+
+
 #: We store the version number inside the `pyproject.toml`:
-pkg_version: str = pkg_resources.get_distribution(pkg_name).version
+pkg_version: str = _get_version(pkg_name)
