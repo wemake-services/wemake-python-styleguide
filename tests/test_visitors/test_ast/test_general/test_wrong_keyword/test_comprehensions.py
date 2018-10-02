eea9beeab91e00b42e546f1afd7813d94d961c09
sobolevn <mail@sobolevn.me>
2018-10-02 19:54:40 +0300

    Refactors async tests for comprehensions

--- tests/test_visitors/test_ast/test_general/test_wrong_keyword/test_comprehensions.py
+++ tests/test_visitors/test_ast/test_general/test_wrong_keyword/test_comprehensions.py
@@ -10,95 +10,68 @@ from wemake_python_styleguide.visitors.ast.keywords import (
 )
 
 # Lists:
-async_wrapper = """
-async def f():
-    {comprehension}
-"""
 
 list_ifs_multiple = """
-nodes = [
-    node
-    {async_stmt}for node in 'abc'
-    if node != 'a' if node != 'b' if node != 'c'
-]
+nodes = [node for node in "abc" if node != "a" if node != "b" if node != "c"]
 """
 
 list_ifs_twice = """
-nodes = [node {async_stmt}for node in 'abc' if node != 'a' if node != 'b']
+nodes = [node for node in "abc" if node != "a" if node != "b"]
 """
 
-list_ifs_single = """
-nodes = [node {async_stmt}for node in 'abc' if node != 'a']
-"""
-
-list_without_ifs = """
-nodes = [node {async_stmt}for node in 'abc']
-"""
+list_ifs_single = 'nodes = [node for node in "abc" if node != "a"]'
+list_without_ifs = 'nodes = [node for node in "abc"]'
 
 # Dicts:
 
 dict_ifs_multiple = """
-nodes = {{
-    xy: xy
-    {async_stmt}for xy in 'abc'
-    if xy != 'a' if xy != 'b' if xy != 'c'
-}}
+nodes = {xy: xy for xy in "abc" if xy != "a" if xy != "b" if xy != "c"}
 """
 
-dict_ifs_twice = """
-nodes = {{xy: xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b'}}
-"""
-
-dict_ifs_single = """
-nodes = {{xy: xy {async_stmt}for xy in 'abc' if xy != 'a'}}
-"""
-
-dict_without_ifs = """
-nodes = {{xy: xy {async_stmt}for xy in 'abc'}}
-"""
+dict_ifs_twice = 'nodes = {xy: xy for xy in "abc" if xy != "a" if xy != "b"}'
+dict_ifs_single = 'nodes = {xy: xy for xy in "abc" if xy != "a"}'
+dict_without_ifs = 'nodes = {xy: xy for xy in "abc"}'
 
 # Generator expressions:
 
 gen_ifs_multiple = """
-nodes = (
-    xy
-    {async_stmt}for xy in 'abc'
-    if xy != 'a' if xy != 'b' if xy != 'c'
-)
+nodes = (xy for xy in "abc" if xy != "a" if xy != "b" if xy != "c")
 """
 
-gen_ifs_twice = """
-nodes = (xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b')
-"""
-
-gen_ifs_single = """
-nodes = (xy {async_stmt}for xy in 'abc' if xy != 'a')
-"""
-
-gen_without_ifs = """
-nodes = (no {async_stmt}for xy in 'abc')
-"""
+gen_ifs_twice = 'nodes = (xy for xy in "abc" if xy != "a" if xy != "b")'
+gen_ifs_single = 'nodes = (xy for xy in "abc" if xy != "a")'
+gen_without_ifs = 'nodes = (no for xy in "abc")'
 
 # Set comprehensions:
 
 set_ifs_multiple = """
-nodes = {{
-    xy
-    {async_stmt}for xy in 'abc'
-    if xy != 'a' if xy != 'b' if xy != 'c'
-}}
+nodes = {xy for xy in "abc" if xy != "a" if xy != "b" if xy != "c"}
 """
 
-set_ifs_twice = """
-nodes = {{xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b'}}
+set_ifs_twice = 'nodes = {xy for xy in "abc" if xy != "a" if xy != "b"}'
+set_ifs_single = 'nodes = {xy for xy in "abc" if xy != "a"}'
+set_without_ifs = 'nodes = {xy for xy in "abc"}'
+
+# Async:
+
+async_list_ifs_multiple = """
+async def wrapper():
+    return [xy async for xy in "abc" if xy != "a" if xy != "b" if xy != "c"]
 """
 
-set_ifs_single = """
-nodes = {{xy {async_stmt}for xy in 'abc' if xy != 'a'}}
+async_list_ifs_twice = """
+async def wrapper():
+    return [xy async for xy in "abc" if xy != "a" if xy != "b"]
 """
 
-set_without_ifs = """
-nodes = {{xy {async_stmt}for xy in 'abc'}}
+async_list_ifs_single = """
+async def wrapper():
+    return [xy async for xy in "abc" if xy != "a"]
+"""
+
+async_list_without_ifs = """
+async def wrapper():
+    return [xy async for xy in "abc"]
 """
 
 
@@ -111,22 +84,16 @@ nodes = {{xy {async_stmt}for xy in 'abc'}}
     gen_without_ifs,
     set_ifs_single,
     set_without_ifs,
+    async_list_ifs_single,
+    async_list_without_ifs,
 ])
-@pytest.mark.parametrize('is_async', [True, False])
 def test_if_keyword_in_comprehension(
     assert_errors,
     parse_ast_tree,
     code,
-    is_async,
     default_options,
 ):
     """Testing that using `if` keyword is allowed."""
-    if is_async:
-        code = code.format(async_stmt='async ')
-        code = code.lstrip()  # remove heading \n symbol
-        code = async_wrapper.format(comprehension=code)
-    else:
-        code = code.format(async_stmt='')
     tree = parse_ast_tree(code)
 
     visitor = WrongListComprehensionVisitor(default_options, tree=tree)
@@ -144,24 +111,17 @@ def test_if_keyword_in_comprehension(
     gen_ifs_twice,
     set_ifs_multiple,
     set_ifs_twice,
+    async_list_ifs_multiple,
+    async_list_ifs_twice,
 ])
-@pytest.mark.parametrize('is_async', [True, False])
 def test_multiple_if_keywords_in_comprehension(
     assert_errors,
     parse_ast_tree,
     code,
-    is_async,
     default_options,
 ):
     """Testing that using multiple `if` keywords is restricted."""
-    if is_async:
-        code = code.format(async_stmt='async ')
-        code = code.lstrip()  # remove heading \n symbol
-        code = async_wrapper.format(comprehension=code)
-    else:
-        code = code.format(async_stmt='')
     tree = parse_ast_tree(code)
-
     visitor = WrongListComprehensionVisitor(default_options, tree=tree)
     visitor.run()
 
