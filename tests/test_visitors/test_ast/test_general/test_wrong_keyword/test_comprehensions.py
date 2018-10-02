eba042a57a23572b603e631e81b8b7f3af2faed5
Alwx <alwxsin@gmail.com>
2018-10-02 16:48:25 +0300

    Test async list comprehensions
    wemake-services/wemake-python-styleguide#173

--- tests/test_visitors/test_ast/test_general/test_wrong_keyword/test_comprehensions.py
+++ tests/test_visitors/test_ast/test_general/test_wrong_keyword/test_comprehensions.py
@@ -10,75 +10,79 @@ from wemake_python_styleguide.visitors.ast.keywords import (
 )
 
 # Lists:
+async_wrapper = """
+async def f():
+    {comprehension}
+"""
 
 list_ifs_multiple = """
-nodes = [node for node in 'abc' if node != 'a' if node != 'b' if node != 'c']
+nodes = [node {async_stmt}for node in 'abc' if node != 'a' if node != 'b' if node != 'c']
 """
 
 list_ifs_twice = """
-nodes = [node for node in 'abc' if node != 'a' if node != 'b']
+nodes = [node {async_stmt}for node in 'abc' if node != 'a' if node != 'b']
 """
 
 list_ifs_single = """
-nodes = [node for node in 'abc' if node != 'a']
+nodes = [node {async_stmt}for node in 'abc' if node != 'a']
 """
 
 list_without_ifs = """
-nodes = [node for node in 'abc']
+nodes = [node {async_stmt}for node in 'abc']
 """
 
 # Dicts:
 
 dict_ifs_multiple = """
-nodes = {xy: xy for xy in 'abc' if xy != 'a' if xy != 'b' if xy != 'c'}
+nodes = {{xy: xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b' if xy != 'c'}}
 """
 
 dict_ifs_twice = """
-nodes = {xy: xy for xy in 'abc' if xy != 'a' if xy != 'b'}
+nodes = {{xy: xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b'}}
 """
 
 dict_ifs_single = """
-nodes = {xy: xy for xy in 'abc' if xy != 'a'}
+nodes = {{xy: xy {async_stmt}for xy in 'abc' if xy != 'a'}}
 """
 
 dict_without_ifs = """
-nodes = {xy: xy for xy in 'abc'}
+nodes = {{xy: xy {async_stmt}for xy in 'abc'}}
 """
 
 # Generator expressions:
 
 gen_ifs_multiple = """
-nodes = (xy for xy in 'abc' if xy != 'a' if xy != 'b' if xy != 'c')
+nodes = (xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b' if xy != 'c')
 """
 
 gen_ifs_twice = """
-nodes = (xy for xy in 'abc' if xy != 'a' if xy != 'b')
+nodes = (xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b')
 """
 
 gen_ifs_single = """
-nodes = (xy for xy in 'abc' if xy != 'a')
+nodes = (xy {async_stmt}for xy in 'abc' if xy != 'a')
 """
 
 gen_without_ifs = """
-nodes = (no for xy in 'abc')
+nodes = (no {async_stmt}for xy in 'abc')
 """
 
 # Set comprehensions:
 
 set_ifs_multiple = """
-nodes = {xy for xy in 'abc' if xy != 'a' if xy != 'b' if xy != 'c'}
+nodes = {{xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b' if xy != 'c'}}
 """
 
 set_ifs_twice = """
-nodes = {xy for xy in 'abc' if xy != 'a' if xy != 'b'}
+nodes = {{xy {async_stmt}for xy in 'abc' if xy != 'a' if xy != 'b'}}
 """
 
 set_ifs_single = """
-nodes = {xy for xy in 'abc' if xy != 'a'}
+nodes = {{xy {async_stmt}for xy in 'abc' if xy != 'a'}}
 """
 
 set_without_ifs = """
-nodes = {xy for xy in 'abc'}
+nodes = {{xy {async_stmt}for xy in 'abc'}}
 """
 
 
@@ -92,13 +96,21 @@ nodes = {xy for xy in 'abc'}
     set_ifs_single,
     set_without_ifs,
 ])
+@pytest.mark.parametrize('is_async', [True, False])
 def test_if_keyword_in_comprehension(
     assert_errors,
     parse_ast_tree,
     code,
+    is_async,
     default_options,
 ):
     """Testing that using `if` keyword is allowed."""
+    if is_async:
+        code = code.format(async_stmt='async ')
+        code = code.lstrip()  # remove heading \n symbol
+        code = async_wrapper.format(comprehension=code)
+    else:
+        code = code.format(async_stmt='')
     tree = parse_ast_tree(code)
 
     visitor = WrongListComprehensionVisitor(default_options, tree=tree)
@@ -117,13 +129,21 @@ def test_if_keyword_in_comprehension(
     set_ifs_multiple,
     set_ifs_twice,
 ])
+@pytest.mark.parametrize('is_async', [True, False])
 def test_multiple_if_keywords_in_comprehension(
     assert_errors,
     parse_ast_tree,
     code,
+    is_async,
     default_options,
 ):
     """Testing that using multiple `if` keywords is restricted."""
+    if is_async:
+        code = code.format(async_stmt='async ')
+        code = code.lstrip()  # remove heading \n symbol
+        code = async_wrapper.format(comprehension=code)
+    else:
+        code = code.format(async_stmt='')
     tree = parse_ast_tree(code)
 
     visitor = WrongListComprehensionVisitor(default_options, tree=tree)
