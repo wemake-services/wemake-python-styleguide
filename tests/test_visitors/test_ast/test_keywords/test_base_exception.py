340b49196e3cbaf0a786e7daf783aa9895fb03dd
Alex Tereshenkov <avtereshenkov@yahoo.com>
2018-10-14 18:07:50 +0200

    refactored test

--- tests/test_visitors/test_ast/test_keywords/test_base_exception.py
+++ tests/test_visitors/test_ast/test_keywords/test_base_exception.py
@@ -9,16 +9,30 @@ from wemake_python_styleguide.visitors.ast.keywords import (
     WrongExceptionTypeVisitor,
 )
 
-use_baseexception = """
+use_base_exception = """
 try:
     execute()
 except BaseException:
     raise
 """
 
+use_except_exception = """
+try:
+    1 / 0
+except Exception:
+    raise
+"""
+
+use_try_except = """
+try:
+    1 / 0
+except:
+    raise
+"""
+
 
 @pytest.mark.parametrize('code', [
-    use_baseexception,
+    use_base_exception,
 ])
 def test_use_base_exception(
     assert_errors, parse_ast_tree, code, default_options,
@@ -32,16 +46,14 @@ def test_use_base_exception(
     assert_errors(visitor, [BaseExceptionViolation])
 
 
+@pytest.mark.parametrize('code', [
+    use_except_exception,
+])
 def test_exception(
-    assert_errors, parse_ast_tree, default_options,
+    assert_errors, parse_ast_tree, code, default_options,
 ):
     """Testing that `except Exception:` is allowed."""
-    tree = parse_ast_tree("""
-    try:
-        1 / 0
-    except Exception:
-        raise
-    """)
+    tree = parse_ast_tree(code)
 
     visitor = WrongExceptionTypeVisitor(default_options, tree=tree)
     visitor.run()
@@ -49,16 +61,14 @@ def test_exception(
     assert_errors(visitor, [])
 
 
+@pytest.mark.parametrize('code', [
+    use_try_except,
+])
 def test_bare_except(
-    assert_errors, parse_ast_tree, default_options,
+    assert_errors, parse_ast_tree, code, default_options,
 ):
     """Testing that bare `except:` is allowed."""
-    tree = parse_ast_tree("""
-    try:
-        1 / 0
-    except:
-        raise
-    """)
+    tree = parse_ast_tree(code)
 
     visitor = WrongExceptionTypeVisitor(default_options, tree=tree)
     visitor.run()
