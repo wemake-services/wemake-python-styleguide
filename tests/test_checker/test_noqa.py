ffea1f3aac64b3046a38660f6b3553767d179398
sobolevn <mail@sobolevn.me>
2018-10-18 14:20:28 +0300

    Fixes noqa example

--- tests/test_checker/test_noqa.py
+++ tests/test_checker/test_noqa.py
@@ -27,6 +27,7 @@ def test_noqa_fixture_disabled(absolute_path):
         'Z113': 1,
 
         'Z220': 1,
+        'Z224': 1,
 
         'Z300': 1,
         'Z302': 1,
--- wemake_python_styleguide/violations/complexity.py
+++ wemake_python_styleguide/violations/complexity.py
@@ -496,7 +496,6 @@ class TooManyForsInComprehensionViolation(ASTViolation):
             for assignment in top_level_assigns
             for target in assignment.targets
             for _ in range(10)
-            if isinstance(target, ast.Name) and is_upper_case_name(target.id)
         ]
 
     Note:
