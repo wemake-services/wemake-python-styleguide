2249f54f1e3b3836a3924fc8b13eb931d577a27e
Timur <timur.makarchuk@gmail.com>
2019-01-07 14:39:52 +0300

    Test improvements
    - Split positive and negative tests into two separate cases
    - Add `mode` param to test async versions as well
    - Wrap test examples in function (to allow usage of `async`)

--- tests/test_visitors/test_ast/test_statements/test_variable_used_outside_of_block.py
+++ tests/test_visitors/test_ast/test_statements/test_variable_used_outside_of_block.py
@@ -17,56 +17,77 @@ def function():
 """
 
 multiple_variables_defined_by_with = """
-with foo.open(), bar.open() as baz, qux.open() as (quux, quuz):
-    print("Hello")
-print(quuz)
+def test_context():
+    with foo.open(), bar.open() as baz, qux.open() as (quux, quuz):
+        print("Hello")
+    print(quuz)
 """
 
 variable_used_outside_for = """
-for ind in range(100):
-    print(ind)
-total = ind + 1
+def test_context():
+    for ind in range(100):
+        print(ind)
+    total = ind + 1
 """
 
 multiple_variables_defined_by_for = """
-for (foo, bar, baz, qux) in call(1):
-    iterate()
-total = baz / 2
+def test_context():
+    for (foo, bar, baz, qux) in call(1):
+        iterate()
+    total = baz / 2
 """
 
 valid_with_usage = """
-with manager as (foo, bar):
-    print(bar)
-print(manager)
+def test_context():
+    with manager as (foo, bar):
+        print(bar)
+    print(manager)
 """
 
 
 valid_for_usage = """
-counter = 0
-for foo in range(100):
-    print(foo)
-    counter = foo
-print(counter)
+def test_context():
+    counter = 0
+    for foo in range(100):
+        print(foo)
+        counter = foo
+    print(counter)
 """
 
 
-@pytest.mark.parametrize('example,violation', [
-    (variable_used_outside_for, [VariableUsedOutsideOfBlockViolation]),
-    (multiple_variables_defined_by_for, [VariableUsedOutsideOfBlockViolation]),
-    (variable_used_outside_with, [VariableUsedOutsideOfBlockViolation]),
-    (multiple_variables_defined_by_with, [VariableUsedOutsideOfBlockViolation]),
-    (valid_for_usage, []),
-    (valid_with_usage, []),
+@pytest.mark.parametrize('example', [
+    variable_used_outside_for,
+    multiple_variables_defined_by_for,
+    variable_used_outside_with,
+    multiple_variables_defined_by_with,
 ])
 def test_detect_violations(
     example,
-    violation,
     assert_errors,
     parse_ast_tree,
     default_options,
+    mode,
 ):
     """Testing that detection of variables being used outside of block."""
-    tree = parse_ast_tree(example)
+    tree = parse_ast_tree(mode(example))
     visitor = VariableUsedOutsideOfBlockVisitor(default_options, tree)
     visitor.run()
-    assert_errors(visitor, violation)
+    assert_errors(visitor, [VariableUsedOutsideOfBlockViolation])
+
+
+@pytest.mark.parametrize('example', [
+    valid_for_usage,
+    valid_with_usage,
+])
+def test_valid_variables_usage(
+    example,
+    assert_errors,
+    parse_ast_tree,
+    default_options,
+    mode,
+):
+    """Testing that detection of variables being used outside of block."""
+    tree = parse_ast_tree(mode(example))
+    visitor = VariableUsedOutsideOfBlockVisitor(default_options, tree)
+    visitor.run()
+    assert_errors(visitor, [])
