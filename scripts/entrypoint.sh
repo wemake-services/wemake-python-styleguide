4593837fe44dfb302ed7c6ae5af1aa6581d48007
sobolevn <mail@sobolevn.me>
2019-09-19 13:51:42 +0300

    Corrects entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-set -e
+
+# Diagnostic output for the passed path:
+echo "Linting path: $1"
 
 # Runs flake8:
-which flake8
-flake8 --help
-flake8 .
 output=$(flake8 "$1")
-
-# Outputs the result to the console:
-echo "$output"
 
 # Sets the output variable for Github Action API:
 echo ::set-output name=output::$output
+
+# Fail the build in case there's any output (means there are violations):
+if [[ "$output" ]]; then
+  echo "$output"
+  exit 1
+fi
