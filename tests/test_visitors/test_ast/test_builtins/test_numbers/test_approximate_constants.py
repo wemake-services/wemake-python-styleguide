b0e398fb9c6cbe3398e9ac0465465cfe0fc89371
Roxe322 <kolomatskiy@bk.ru>
2019-10-06 23:07:01 +0300

    fix errors

--- tests/test_visitors/test_ast/test_builtins/test_numbers/test_approximate_constants.py
+++ tests/test_visitors/test_ast/test_builtins/test_numbers/test_approximate_constants.py
@@ -9,12 +9,12 @@ from wemake_python_styleguide.visitors.ast.builtins import WrongNumberVisitor
 
 
 @pytest.mark.parametrize('variable_name,variable_value', [
-    ('pi', 3.14),
-    ('tau', 6.28),
-    ('e', 2.718),
-    ('my_e', 2.72),
-    ('my_pi', 3.141),
-    ('my_tau', 6.282),
+    ('pi', '3.14'),
+    ('tau', '6.28'),
+    ('e', '2.718'),
+    ('my_e', '2.72'),
+    ('my_pi', '3.141'),
+    ('my_tau', '6.282'),
 ])
 def test_violation_on_approximate_constants(
     assert_errors,
