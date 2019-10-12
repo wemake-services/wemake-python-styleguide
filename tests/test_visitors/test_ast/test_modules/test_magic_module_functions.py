7484f0b0787f17bf570d6e86e47d40e2808b22ff
raghav-ys <56265370+raghav-ys@users.noreply.github.com>
2019-10-12 16:55:20 +0530

    Update test_magic_module_functions.py

--- tests/test_visitors/test_ast/test_modules/test_magic_module_functions.py
+++ tests/test_visitors/test_ast/test_modules/test_magic_module_functions.py
@@ -29,4 +29,21 @@ def test_wrong_magic_used(
     visitor = MagicModuleFunctionsVisitor(default_options, tree=tree)
     visitor.run()
 
+    assert_errors(visitor, [])
+    
+      
+@pytest.mark.parametrize('function_names', MAGIC_MODULE_NAMES_BLACKLIST)
+def test_right_magic_used(
+    assert_errors,
+    parse_ast_tree,
+    options,
+    function_names,
+):
+    """Testing that some magic methods are restricted."""
+    tree = parse_ast_tree(regular_function.format(function_names))
+    option_values = options(i_control_code=True)
+    
+    visitor = MagicModuleFunctionsVisitor(options_values, tree=tree)
+    visitor.run()
+
     assert_errors(visitor, [BadMagicModuleFunctionViolation])
