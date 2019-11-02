afa977b26b1d7bddccedd637035421014db12192
Luca Ionescu <lucaionescu@users.noreply.github.com>
2019-11-02 17:21:53 +0100

    move node iteration to logic/ directory. remove cast to Set.

---
+++ wemake_python_styleguide/logic/calls.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import ast
+from typing import Iterable, Optional
+
+
+def _chained_item(iterator: ast.AST) -> Optional[ast.Call]:
+    if isinstance(iterator, ast.Call):
+        for child in ast.iter_child_nodes(iterator):
+            if isinstance(child, ast.Call):
+                return iterator
+    elif isinstance(iterator, ast.Expr) and isinstance(iterator.value, ast.Call):
+        return iterator.value
+    return None
+
+
+def parts(node: ast.Call) -> Iterable[ast.Call]:
+    """
+    Returns all consecutive function calls.
+    """
+    iterator: ast.Call = node
+
+    while True:
+        yield iterator
+
+        chained_item = _chained_item(iterator)
+        if chained_item is None:
+            return
+        iterator = chained_item
--- wemake_python_styleguide/visitors/ast/complexity/calls.py
+++ wemake_python_styleguide/visitors/ast/complexity/calls.py
@@ -2,11 +2,11 @@
 
 import ast
 from itertools import takewhile
-from typing import cast, Set
-from typing import Iterable, Optional
+from typing import Set
 
 from typing_extensions import final
 
+from wemake_python_styleguide.logic.calls import parts
 from wemake_python_styleguide.violations.complexity import (
     TooLongCallChainViolation,
 )
@@ -15,7 +15,7 @@ from wemake_python_styleguide.visitors.base import BaseNodeVisitor
 
 @final
 class CallChainsVisitor(BaseNodeVisitor):
-    """Counts access number for expressions."""
+    """Counts number of consecutive calls."""
 
     def __init__(self, *args, **kwargs) -> None:
         """Keeps visited calls to not visit them again."""
@@ -36,33 +36,13 @@ class CallChainsVisitor(BaseNodeVisitor):
     def _is_call(self, node: ast.AST) -> bool:
         return isinstance(node, ast.Call)
 
-    def _chained_item(self, iterator: ast.AST) -> Optional[ast.AST]:
-        if isinstance(iterator, ast.Call):
-            for child in ast.iter_child_nodes(iterator):
-                if isinstance(child, ast.Call):
-                    return iterator
-        elif isinstance(iterator, ast.Expr) and isinstance(iterator.value, ast.Call):
-            return iterator.value
-        return None
-
-    def _parts(self, node: ast.Call) -> Iterable[ast.AST]:
-        iterator: ast.AST = node
-
-        while True:
-            yield iterator
-
-            chained_item = self._chained_item(iterator)
-            if chained_item is None:
-                return
-            iterator = chained_item
-
     def _check_consecutive_call_number(self, node: ast.Call) -> None:
         if node in self._visited_calls:
             return
 
-        consecutive_calls = cast(Set[ast.Call], set(takewhile(
-            self._is_call, self._parts(node),
-        )))
+        consecutive_calls = set(takewhile(
+            self._is_call, parts(node),
+        ))
 
         self._visited_calls.update(consecutive_calls)
         num_of_calls = len(self._visited_calls)
