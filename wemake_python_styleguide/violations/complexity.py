68f00d29c23cc7c5c0092531a270cbbd8717ef65
Pavel Druzhinin <34269347+pvldruzhinin@users.noreply.github.com>
2019-12-17 18:26:43 +0300

    Update docstring TooLongTryBodyViolation() (#1078)
    * Update See also link for TooLongTryBodyViolation()
    
    * Fixed excess whitespaces

--- wemake_python_styleguide/violations/complexity.py
+++ wemake_python_styleguide/violations/complexity.py
@@ -900,6 +900,9 @@ class TooLongTryBodyViolation(ASTViolation):
         Default:
         :str:`wemake_python_styleguide.options.defaults.MAX_TRY_BODY_LENGTH`
 
+    See also:
+        https://adamj.eu/tech/2019/10/02/limit-your-try-clauses-in-python/
+
     .. versionadded:: 0.12.0
 
     """
