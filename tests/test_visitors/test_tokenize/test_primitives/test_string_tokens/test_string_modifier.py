a0303c3dd11718802b133530b4f11d7f9639fac2
Mema5 <maelmadon@wanadoo.fr>
2020-02-24 12:32:10 +0100

    test: new tests for rule WPS357. Not passing for escaped quote. PR #1178

--- tests/test_visitors/test_tokenize/test_primitives/test_string_tokens/test_string_modifier.py
+++ tests/test_visitors/test_tokenize/test_primitives/test_string_tokens/test_string_modifier.py
@@ -3,15 +3,18 @@
 import pytest
 
 from wemake_python_styleguide.violations.consistency import (
+    RawStringNotNeededViolation,
     UppercaseStringModifierViolation,
 )
 from wemake_python_styleguide.visitors.tokenize.primitives import (
-    RawStringNotNeededViolation,
     WrongStringTokenVisitor,
 )
 
 
 @pytest.mark.parametrize('modifier', [
+    'r',
+    'rb',
+    'fr',
     'b',
     'f',
     '',  # special case, no modifier is used
@@ -38,67 +41,11 @@ def test_correct_prefix(
     visitor = WrongStringTokenVisitor(default_options, file_tokens=file_tokens)
     visitor.run()
 
-    assert_errors(visitor, [])
-
-
-@pytest.mark.parametrize('modifier', [
-    'r',
-    'rb',
-    'fr',
-])
-@pytest.mark.parametrize('primitive', [
-    '{0}"\n"',
-    "{0}'\n'",
-    '{0}"Big \ntext"',
-    "{0}'Format \n123'",
-])
-def test_correct_prefix_raw_string(
-    parse_tokens,
-    assert_errors,
-    default_options,
-    primitives_usages,
-    primitive,
-    modifier,
-    mode,
-):
-    """Ensures that correct prefixes work."""
-    string = primitive.format(modifier)
-    file_tokens = parse_tokens(mode(primitives_usages.format(string)))
-
-    visitor = WrongStringTokenVisitor(default_options, file_tokens=file_tokens)
-    visitor.run()
-
-    assert_errors(visitor, [])
-
-
-@pytest.mark.parametrize('modifier', [
-    'r',
-    'rb',
-    'fr',
-])
-@pytest.mark.parametrize('primitive', [
-    '{0}""',
-    "{0}''",
-    '{0}"Big text"',
-    "{0}'Format 123'",
-])
-def test_wrong_raw_string(
-    parse_tokens,
-    assert_errors,
-    default_options,
-    primitives_usages,
-    primitive,
-    modifier,
-    mode,
-):
-    """Ensures that usage of raw string is forbidden if no backslash."""
-    string = primitive.format(modifier)
-    file_tokens = parse_tokens(mode(primitives_usages.format(string)))
-
-    visitor = WrongStringTokenVisitor(default_options, file_tokens=file_tokens)
-    visitor.run()
-
-    assert_errors(visitor, [RawStringNotNeededViolation])
+    assert_errors(
+        visitor,
+        [],
+        ignored_types=(RawStringNotNeededViolation,),
+    )
 
 
 @pytest.mark.parametrize('modifier', [
@@ -170,3 +117,46 @@ def test_uppercase_prefix_multiple(
         UppercaseStringModifierViolation,
         RawStringNotNeededViolation,
     ])
+
+
+@pytest.mark.parametrize('raw_strings', [
+    "{0}'Carriage return \r'",
+    "{0}'Escaped \' quote'",
+    "{0}'Escaped ab \t'",
+    "{0}'Escaped backslash \\'",
+])
+def test_necessary_raw_string(
+    parse_tokens,
+    assert_errors,
+    default_options,
+    raw_strings,
+):
+    """Ensures that correct usage of raw string works."""
+    string = raw_strings.format('r')
+    file_tokens = parse_tokens(string)
+
+    visitor = WrongStringTokenVisitor(default_options, file_tokens=file_tokens)
+    visitor.run()
+
+    assert_errors(visitor, [])
+
+
+@pytest.mark.parametrize('raw_strings', [
+    "{0}'No escaped caracter'",
+    '{0}"Here neither"',
+    "{0}'''Not here as well'''",
+])
+def test_unnecessary_raw_string(
+    parse_tokens,
+    assert_errors,
+    default_options,
+    raw_strings,
+):
+    """Ensures that usage of raw string is forbidden if no backslash."""
+    string = raw_strings.format('r')
+    file_tokens = parse_tokens(string)
+
+    visitor = WrongStringTokenVisitor(default_options, file_tokens=file_tokens)
+    visitor.run()
+
+    assert_errors(visitor, [RawStringNotNeededViolation])
