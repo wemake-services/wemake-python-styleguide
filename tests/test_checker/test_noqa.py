9a54ebb6ecc9a2472493411a7441ff4c199bbea1
Gwin73 <adrianwesterberg@hotmail.com>
2020-02-25 13:08:27 +0100

    Test: make requirested PR changes, right know linting fails with I001

--- tests/test_checker/test_noqa.py
+++ tests/test_checker/test_noqa.py
@@ -60,7 +60,7 @@ SHOULD_BE_RAISED = types.MappingProxyType({
     'WPS102': 0,  # logically unacceptable.
     'WPS110': 3,
     'WPS111': 1,
-    'WPS112': 5,
+    'WPS112': 1,
     'WPS113': 1,
     'WPS114': 1,
     'WPS115': 1,
@@ -262,7 +262,7 @@ SHOULD_BE_RAISED = types.MappingProxyType({
     'WPS611': 1,
     'WPS612': 1,
     'WPS613': 1,
-    'WPS614': 12,
+    'WPS614': 1,
 })
 
 # Violations which may be tweaked by `i_control_code` option:
---
+++ tests/test_visitors/test_ast/test_classes/test_getter_setter.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from wemake_python_styleguide.visitors.ast.classes import (
+    WrongClassVisitor,
+)
+from wemake_python_styleguide.violations.oop import (
+    UnpythonicGetterSetterViolation,
+)
+
+module_getter_and_setter = """
+attribute = 1
+
+def get_attribute():
+    ...
+
+def set_attribute():
+    ...
+"""
+
+static_getter_and_setter = """
+attribute = 1
+
+class Test(object):
+    @staticmethod
+    def get_attribute():
+        ...
+
+    @staticmethod
+    def set_attribute():
+        ...
+"""
+
+property_getter_and_setter = """
+class Test(object):
+    def __init__(self):
+        self.attribute = 1
+
+    @property
+    def get_attribute(self):
+        ...
+
+    @property.setter
+    def set_attribute(self):
+        ...
+"""
+
+child_getter_and_setter = """
+class TestParent(object):
+    def __init__(self):
+        self.attribute = 1
+
+class TestChild(TestParent):
+    def get_attribute(self):
+        ...
+
+    def set_attribute(self):
+        ...
+"""
+
+nested_getter_and_setter = """
+class Template(object):
+    def __init__(self):
+        self.attribute = 1
+
+    def some_function(self):
+        def get_attribute(self):
+            ...
+        def set_attribute(self):
+            ...
+        get_attribute(self)
+"""
+
+class_attribute_template = """
+class Template(object):
+    def __init__(self):
+        self.{0} = 1
+
+    {1}
+    def {2}(self):
+        ...
+"""
+
+
+@pytest.mark.parametrize('code', [
+    module_getter_and_setter,
+    static_getter_and_setter,
+    property_getter_and_setter,
+    child_getter_and_setter,
+    nested_getter_and_setter,
+])
+def test_property_getter_and_setter(
+    assert_errors,
+    parse_ast_tree,
+    default_options,
+    code,
+    mode,
+):
+    """Testing that attribute, getter and setter is allowed outside of class."""
+    tree = parse_ast_tree(mode(code))
+
+    visitor = WrongClassVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [])
+
+
+@pytest.mark.parametrize(('first', 'second', 'third'), [
+    ('attribute', '', 'get_attribute_some'),
+    ('attribute', '', 'some_get_attribute'),
+    ('attribute', '', 'get_some_attribute'),
+    ('attribute', '', 'attribute_get'),
+    ('some_attribute', '', 'get_attribute'),
+    ('attribute_some', '', 'get_attribute'),
+])
+def test_nonmatching_attribute_getter_setter(
+    assert_errors,
+    parse_ast_tree,
+    default_options,
+    first,
+    second,
+    third,
+    mode,
+):
+    """Testing that non matching attribute and getter/setter is allowed."""
+    test_instance = class_attribute_template.format(first, second, third)
+    tree = parse_ast_tree(mode(test_instance))
+
+    visitor = WrongClassVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [])
+
+
+@pytest.mark.parametrize(('first', 'second', 'third'), [
+    ('attribute', '', 'get_attribute'),
+    ('_attribute', '', 'get_attribute'),
+    ('__attribute', '', 'get_attribute'),
+    ('attribute', '@classmethod', 'set_attribute'),
+    ('_attribute', '@classmethod', 'set_attribute'),
+    ('__attribute', '@classmethod', 'set_attribute'),
+])
+def test_instance_and_class_getter_setter(
+    assert_errors,
+    parse_ast_tree,
+    default_options,
+    first,
+    second,
+    third,
+    mode,
+):
+    """Testing that instance/class attribute and getter/setter is prohibited."""
+    test_instance = class_attribute_template.format(first, second, third)
+    tree = parse_ast_tree(mode(test_instance))
+
+    visitor = WrongClassVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [UnpythonicGetterSetterViolation])
