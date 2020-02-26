9dbd84fe5d53842ca5b71fefe9586ae842aae460
hanzhsu <emily_1999@link.cuhk.edu.hk>
2020-02-26 17:52:32 +0100

    Add WPS452 to test_noqa.py #5

--- tests/test_checker/test_noqa.py
+++ tests/test_checker/test_noqa.py
@@ -217,6 +217,7 @@ SHOULD_BE_RAISED = types.MappingProxyType({
     'WPS449': 1,
     'WPS450': 1,
     'WPS451': 0,  # defined in version specific table.
+    'WPS452': 0,  # logically unacceptable.
 
     'WPS500': 1,
     'WPS501': 1,
