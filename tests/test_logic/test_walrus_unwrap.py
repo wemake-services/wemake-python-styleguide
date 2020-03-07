c619d96c7c394ce7b3f2c48889bd3dd118ced0fd
Nikita Sobolev <mail@sobolevn.me>
2020-03-08 01:59:48 +0300

    Update test_walrus_unwrap.py

--- tests/test_logic/test_walrus_unwrap.py
+++ tests/test_logic/test_walrus_unwrap.py
@@ -6,6 +6,11 @@ from wemake_python_styleguide.logic.walrus import get_assigned_expr
 
 
 @pytest.mark.parametrize('code', [
+    'x = 1',
+    '"a"',
+    'def some(): ...',
+    'if other: ...',
+    'pass',
     pytest.param(
         '(x := call())',
         marks=pytest.mark.skipif(not PY38, reason='walrus appeared in 3.8'),
@@ -16,22 +21,6 @@ from wemake_python_styleguide.logic.walrus import get_assigned_expr
     ),
 ])
 def test_get_assigned_expr(code, parse_ast_tree):
-    """Returns the value of NamedExpr, the node itself otherwise."""
-    node = parse_ast_tree(code)
-
-    result_node = get_assigned_expr(node.body[0].value)
-
-    assert isinstance(result_node, NamedExpr)
-
-
-@pytest.mark.parametrize('code', [
-    'x = 1',
-    '"a"',
-    'def some(): ...',
-    'if other: ...',
-    'pass',
-])
-def test_get_regular_node(code, parse_ast_tree):
     """Returns the value of NamedExpr, the node itself otherwise."""
     node = parse_ast_tree(code)
 
