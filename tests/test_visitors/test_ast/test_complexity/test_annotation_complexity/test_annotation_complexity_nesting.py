475a16600acd54c3e087acf07ff65767831cf46b
Andrey <leksus@leksus.info>
2020-03-07 13:59:04 +0300

    Add test cases for expression with attributes (#1244)
    Check expressions with attribute with dot notation in annotation complexity.
    
    Fix #1187

--- tests/test_visitors/test_ast/test_complexity/test_annotation_complexity/test_annotation_complexity_nesting.py
+++ tests/test_visitors/test_ast/test_complexity/test_annotation_complexity/test_annotation_complexity_nesting.py
@@ -69,6 +69,9 @@ class Test(object):
     'Callable[[str, int], int]',
     'List[List[int]]',
     '"String Annontation"',
+    'typing.int',
+    'typing.List[int]',
+    'typing.List["typing.Tuple[int]"]',
 ])
 def test_correct_annotations(
     assert_errors,
@@ -112,6 +115,10 @@ def test_correct_annotations(
     'Dict[int, "Tuple[List[List[str]], ...]"]',
     'Dict[int, Tuple["List[List[str]]", ...]]',
     'Dict[int, Tuple[List["List[str]"], ...]]',
+
+    '"typing.List[typing.List[typing.List[typing.List[int]]]]"',
+    'typing.Dict["str", "typing.List[typing.List[typing.List[int]]]"]',
+    'typing.Tuple[typing.List[typing.Dict[int, "typing.List[str]"]]]',
 ])
 def test_complex_annotations(
     assert_errors,
