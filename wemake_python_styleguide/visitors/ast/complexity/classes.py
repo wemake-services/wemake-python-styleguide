a9854617879d8154d959e18e16ae67dc09dfea61
Ruwaid Louis <ruwaid@kth.se>
2020-03-08 00:00:31 +0100

    Refactor: Change method to apply for new implementation

--- wemake_python_styleguide/visitors/ast/complexity/classes.py
+++ wemake_python_styleguide/visitors/ast/complexity/classes.py
@@ -58,7 +58,7 @@ class ClassComplexityVisitor(BaseNodeVisitor):
             )
 
     def _check_public_attributes(self, node: ast.ClassDef) -> None:
-        _, instance_attributes = classes.get_attributes(node)
+        _, instance_attributes = classes.get_attributes(node, include_annotated=False)
         attrs_count = len({
             attr.attr
             for attr in instance_attributes
