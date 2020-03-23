09097eb8dd6de860a729acae8cb2c407e615c23f
sobolevn <mail@sobolevn.me>
2020-03-24 01:50:23 +0300

    Adds --baseline

--- tests/test_patches/test_baseline/test_baseline_option/test_baseline_command.py
+++ tests/test_patches/test_baseline/test_baseline_option/test_baseline_command.py
@@ -1,5 +1,5 @@
 """
-We use this example together with the existing baseline.
+We use this test to ensure that ``--baseline`` works correctly.
 
 Here are several important things about this example:
 
@@ -10,15 +10,23 @@ Here are several important things about this example:
 
 3. There's a unique violation: ``WPS303``
 
+We also have the second file around with just one ``WPS304`` inside.
+
 All violations in this example are covered by the baseline.
 """
 
 import os
 import subprocess
 
-baseline = """
-{
+import pytest
+
+from wemake_python_styleguide.logic.baseline import BASELINE_FILE
+
+baseline = """{
   "paths": {
+    "other_wrong.py": {
+      "e61cb3c3de1cbdac603069903e4af07c": 1
+    },
     "wrong.py": {
       "132954ef45e1a84ab72bb6e30126a117": 1,
       "71b49f6407bbc09ac76c372014207dfb": 1,
@@ -26,15 +34,23 @@ baseline = """
       "dd2402e2213add848d53f8580452417e": 2
     }
   }
-}
-"""
+}"""
 
-code_template = """
+# Templates:
+
+wrong_template = """
 value =1
 result= 2
 undescored_number = 10_0
 {0}
 """
+
+wrong_other = 'partial = .5'
+
+# Filenames:
+
+filename_wrong = 'wrong.py'
+filename_other = 'other_wrong.py'
 
 
 def _assert_output(output: str, errors):
@@ -42,9 +58,11 @@ def _assert_output(output: str, errors):
         assert output.count(error_code) == error_count
 
 
-def test_without_baseline(make_file):
-    """End-to-End test for no baseline, regular mode."""
-    filename = make_file('wrong.py', code_template.format(''))
+def test_without_baseline(make_file, read_file):
+    """End-to-End test for no baseline yet, initial mode."""
+    filename = make_file(filename_wrong, wrong_template.format(''))
+    make_file(filename_other, wrong_other)
+    cwd = os.path.dirname(filename)
 
     process = subprocess.Popen(
         [
@@ -53,24 +71,33 @@ def test_without_baseline(make_file):
             '--baseline',
             '--select',
             'WPS,E',
-            'wrong.py',
+            filename_wrong,
+            filename_other,
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
         encoding='utf8',
-        cwd=os.path.dirname(filename),
+        cwd=cwd,
     )
     output, _ = process.communicate()
 
-    _assert_output(output, {'WPS110': 2, 'WPS303': 1, 'E225': 2})
+    _assert_output(output, {'WPS110': 2, 'WPS303': 1, 'WPS304': 1, 'E225': 2})
     assert process.returncode == 1
+    assert read_file(os.path.join(cwd, BASELINE_FILE)) == baseline
 
 
-def test_with_baseline(make_file):
+@pytest.mark.parametrize('files_to_check', [
+    (filename_wrong,),
+    (filename_wrong, filename_other),
+    (filename_wrong, 'missing.py'),
+    (filename_wrong, filename_other, 'missing.py'),
+])
+def test_with_baseline(make_file, read_file, files_to_check):
     """End-to-End test for baseline generation."""
-    filename = make_file('wrong.py', code_template.format(''))
-    make_file('.flake8-baseline.json', baseline)
+    filename = make_file(filename_wrong, wrong_template.format(''))
+    make_file(filename_other, wrong_other)
+    baseline_path = make_file(BASELINE_FILE, baseline)
 
     process = subprocess.Popen(
         [
@@ -79,7 +106,7 @@ def test_with_baseline(make_file):
             '--baseline',
             '--select',
             'WPS,E',
-            'wrong.py',
+            *files_to_check,
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -91,12 +118,13 @@ def test_with_baseline(make_file):
 
     assert output == ''
     assert process.returncode == 0
+    assert read_file(baseline_path) == baseline
 
 
-def test_with_baseline_empty(make_file):
+def test_with_baseline_empty(make_file, read_file):
     """End-to-End test that removed violations are fine."""
-    filename = make_file('wrong.py', '_SOME_CONSTANT = 1')
-    make_file('.flake8-baseline.json', baseline)
+    filename = make_file(filename_wrong, '_SOME_CONSTANT = 1')
+    baseline_path = make_file(BASELINE_FILE, baseline)
 
     process = subprocess.Popen(
         [
@@ -105,7 +133,7 @@ def test_with_baseline_empty(make_file):
             '--baseline',
             '--select',
             'WPS,E',
-            'wrong.py',
+            filename_wrong,
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -117,12 +145,13 @@ def test_with_baseline_empty(make_file):
 
     assert output == ''
     assert process.returncode == 0
+    assert read_file(baseline_path) == baseline
 
 
-def test_with_baseline_new_violations(make_file):
+def test_with_baseline_new_violations(make_file, read_file):
     """End-to-End test to test that baseline still generates new violations."""
-    filename = make_file('wrong.py', code_template.format('x = 1'))
-    make_file('.flake8-baseline.json', baseline)
+    filename = make_file(filename_wrong, wrong_template.format('x = 1'))
+    baseline_path = make_file(BASELINE_FILE, baseline)
 
     process = subprocess.Popen(
         [
@@ -131,7 +160,7 @@ def test_with_baseline_new_violations(make_file):
             '--baseline',
             '--select',
             'WPS,E',
-            'wrong.py',
+            filename_wrong,
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -143,13 +172,14 @@ def test_with_baseline_new_violations(make_file):
 
     _assert_output(output, {'WPS111': 1})
     assert process.returncode == 1
+    assert read_file(baseline_path) == baseline
 
 
-def test_with_baseline_new_files(make_file):
+def test_with_baseline_new_correct_files(make_file, read_file):
     """End-to-End test to test that baseline still generates new violations."""
-    filename = make_file('wrong.py', code_template.format(''))
+    filename = make_file(filename_wrong, wrong_template.format(''))
     make_file('correct.py', 'SOME_CONSTANT = 1')
-    make_file('.flake8-baseline.json', baseline)
+    baseline_path = make_file(BASELINE_FILE, baseline)
 
     process = subprocess.Popen(
         [
@@ -158,7 +188,7 @@ def test_with_baseline_new_files(make_file):
             '--baseline',
             '--select',
             'WPS,E',
-            'wrong.py',
+            filename_wrong,
             'correct.py',
         ],
         stdout=subprocess.PIPE,
@@ -171,3 +201,34 @@ def test_with_baseline_new_files(make_file):
 
     assert output == ''
     assert process.returncode == 0
+    assert read_file(baseline_path) == baseline
+
+
+def test_with_baseline_new_wrong_files(make_file, read_file):
+    """End-to-End test to test that baseline still generates new violations."""
+    filename = make_file(filename_wrong, wrong_template.format(''))
+    new_wrong = make_file('new_wrong.py', 'wrong__name = 1')
+    baseline_path = make_file(BASELINE_FILE, baseline)
+
+    process = subprocess.Popen(
+        [
+            'flake8',
+            '--isolated',
+            '--baseline',
+            '--select',
+            'WPS,E',
+            filename_wrong,
+            'correct.py',
+            new_wrong,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        encoding='utf8',
+        cwd=os.path.dirname(filename),
+    )
+    output, _ = process.communicate()
+
+    _assert_output(output, {'WPS116': 1})
+    assert process.returncode == 1
+    assert read_file(baseline_path) == baseline
