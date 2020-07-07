7c54467b467c8b6538bc9b963fb8dbb3bd960141
Sam Bull <sam@kopernio.com>
2020-07-07 15:32:21 +0100

    Test fixes.

--- tests/test_patches/test_baseline/test_baseline_option/test_baseline_command.py
+++ tests/test_patches/test_baseline/test_baseline_option/test_baseline_command.py
@@ -325,6 +325,6 @@ def test_missing_baseline(make_file, read_file):
 
     assert output == (
         'ERROR: No baseline file found ' +
-        '(you can create one with --create-baseline).'
+        '(you can create one with --create-baseline).\n'
     )
     assert returncode == -2
