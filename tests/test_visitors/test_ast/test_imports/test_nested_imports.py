30df63ef645b65d5507617833624be146645860a
Kasia <katarzyna.jagiela@tylko.com>
2020-07-18 14:15:40 +0200

    Correct test docstring

--- tests/test_visitors/test_ast/test_imports/test_nested_imports.py
+++ tests/test_visitors/test_ast/test_imports/test_nested_imports.py
@@ -82,7 +82,11 @@ def test_nested_import(assert_errors, parse_ast_tree, code, default_options):
     typing_type_checking_import,
 ])
 def test_regular_imports(assert_errors, parse_ast_tree, code, default_options):
-    """Testing that regular imports are allowed."""
+    """Testing imports that are allowed.
+
+    Regular imports are allowed.
+    Imports nested inside the TYPE_CHECKING check are allowed.
+    """
     tree = parse_ast_tree(code)
 
     visitor = WrongImportVisitor(default_options, tree=tree)
