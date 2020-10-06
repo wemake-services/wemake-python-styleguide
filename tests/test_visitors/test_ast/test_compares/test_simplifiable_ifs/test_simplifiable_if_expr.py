333532dd14fa266b68edd2bb319d90f8fd3f9643
Gerard Otero <jerry.om.95@gmail.com>
2020-10-06 13:16:26 +0200

    Tests for 502 refactoring made

--- tests/test_visitors/test_ast/test_compares/test_simplifiable_ifs/test_simplifiable_if_expr.py
+++ tests/test_visitors/test_ast/test_compares/test_simplifiable_ifs/test_simplifiable_if_expr.py
@@ -9,15 +9,35 @@ from wemake_python_styleguide.visitors.ast.compares import (
 
 if_expression = '{0} if some() else {1}'
 
+conditional_statement = """
+if some_condition:
+    {0}
+else:
+    {1}
+"""
 
-@pytest.mark.parametrize('comparators', [
+early_returning_conditional_statement = """
+if some_condition:
+    {0}
+{1}
+"""
+
+not_simplifiable_comparators = [
     ('variable', '"test"'),
     ('12', 'variable.call()'),
     ('False', 'len(variable)'),
     ('False', 'None'),
     ('True', '222'),
     ('True', 'None'),
-])
+]
+
+simplifiable_comparators = [
+    ('True', 'False'),
+    ('False', 'True'),
+]
+
+
+@pytest.mark.parametrize('comparators', not_simplifiable_comparators)
 def test_not_simplifiable_exp(
     assert_errors,
     parse_ast_tree,
@@ -33,10 +53,7 @@ def test_not_simplifiable_exp(
     assert_errors(visitor, [])
 
 
-@pytest.mark.parametrize('comparators', [
-    ('True', 'False'),
-    ('False', 'True'),
-])
+@pytest.mark.parametrize('comparators', simplifiable_comparators)
 def test_simplifiable_exp(
     assert_errors,
     parse_ast_tree,
@@ -50,3 +67,75 @@ def test_simplifiable_exp(
     visitor.run()
 
     assert_errors(visitor, [SimplifiableIfViolation])
+
+
+@pytest.mark.parametrize('comparators', simplifiable_comparators)
+def test_simplifiable_conditional_statement(
+    assert_errors,
+    parse_ast_tree,
+    comparators,
+    default_options
+):
+    """Test that the conditional statements can be simplified to just returning the condition."""
+    tree = parse_ast_tree(conditional_statement.format(*comparators))
+
+    visitor = WrongConditionalVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [SimplifiableIfViolation])
+
+
+@pytest.mark.parametrize('comparators', not_simplifiable_comparators)
+def test_not_simplifiable_conditional_statement(
+    assert_errors,
+    parse_ast_tree,
+    comparators,
+    default_options
+):
+    """
+    Test that the conditional statements can not be simplified to just returning the condition.
+    """
+    tree = parse_ast_tree(conditional_statement.format(*comparators))
+
+    visitor = WrongConditionalVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [])
+
+
+@pytest.mark.parametrize('comparators', simplifiable_comparators)
+def test_simplifiable_early_returning_conditional_statement(
+    assert_errors,
+    parse_ast_tree,
+    comparators,
+    default_options
+):
+    """
+    Test that the early returning conditional statements can be simplified to just returning the
+    condition.
+    """
+    tree = parse_ast_tree(conditional_statement.format(*comparators))
+
+    visitor = WrongConditionalVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [SimplifiableIfViolation])
+
+
+@pytest.mark.parametrize('comparators', not_simplifiable_comparators)
+def test_not_simplifiable_early_returning_conditional_statement(
+    assert_errors,
+    parse_ast_tree,
+    comparators,
+    default_options
+):
+    """
+    Test that the early returning conditional statements can not be simplified to just returning the
+    condition.
+    """
+    tree = parse_ast_tree(conditional_statement.format(*comparators))
+
+    visitor = WrongConditionalVisitor(default_options, tree=tree)
+    visitor.run()
+
+    assert_errors(visitor, [])
