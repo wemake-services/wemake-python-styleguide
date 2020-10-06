e373c004cc711cf216b16e896c424e4efa6bdf97
Gerard Otero <jerry.om.95@gmail.com>
2020-10-07 00:46:02 +0200

    Added documentation

--- wemake_python_styleguide/violations/refactoring.py
+++ wemake_python_styleguide/violations/refactoring.py
@@ -183,14 +183,15 @@ class SimplifiableIfViolation(ASTViolation):
     Forbid simplifiable ``if`` conditions.
 
     Reasoning:
-        This complex construction can cause frustration among other developers.
-        It is longer, more verbose, and more complex.
+        These complex constructions can cause frustration among other
+        developers. They are longer, more verbose, and more complex.
 
     Solution:
-        Use ``bool()`` to convert test values to boolean values.
-        Or just leave it as it is in case
-        when your test already returns a boolean value.
+        Either use ``bool()`` to convert test values to boolean values, or just
+        leave it as it is in case your test already returns a boolean value.
         Use can also use ``not`` keyword to switch boolean values.
+        In case it is at the end of a function, instead of testing the condition
+        and returning a boolean, return the condition.
 
     Example::
 
@@ -201,11 +202,37 @@ class SimplifiableIfViolation(ASTViolation):
         # Wrong:
         my_bool = True if some_call() else False
 
+        # In functions:
+        # Correct:
+        def some_function():
+            return some_condition
+
+        def some_function():
+            return not (some_condition and other_condition)
+
+        def some_function():
+            if some_condition:
+                return True
+            other_function()
+            return False
+
+        # Wrong:
+        def some_function():
+        if some_condition:
+            return True
+        else:
+            return False
+
+        def some_function():
+        if some_condition:
+            return False
+        return True
+
     We only check ``if`` nodes where ``True`` and ``False`` values are used.
     We check both ``if`` nodes and ``if`` expressions.
 
     .. versionadded:: 0.7.0
-    .. versionchanged:: 0.11.0
+    .. versionchanged:: 0.15.0
 
     """
 
