316423eb67e25a7b477e90853bc4596cd704a7fb
sobolevn <mail@sobolevn.me>
2020-11-03 12:03:12 +0300

    Fixes sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -20,32 +20,6 @@ import tomlkit
 sys.path.insert(0, os.path.abspath('..'))
 
 
-# -- Monkeypatches -----------------------------------------------------------
-
-def _monkeypatch(cls):
-    """Decorator to monkey-patch methods."""
-    def decorator(func):
-        method = func.__name__
-        old = getattr(cls, method)
-        setattr(
-            cls,
-            method,
-            lambda inst, *args, **kwargs: func(inst, old, *args, **kwargs),
-        )
-    return decorator
-
-
-# workaround until https://github.com/miyakogi/m2r/pull/55 is merged
-@_monkeypatch(sphinx.registry.SphinxComponentRegistry)
-def add_source_parser(self, _old_add_source_parser, *args, **kwargs):
-    """This function changed in sphinx v3.0, we need to fix it back."""
-    # signature is (parser: Type[Parser], **kwargs), but m2r expects
-    # the removed (str, parser: Type[Parser], **kwargs).
-    if isinstance(args[0], str):
-        args = args[1:]
-    return _old_add_source_parser(self, *args, **kwargs)
-
-
 # -- Project information -----------------------------------------------------
 
 def _get_project_meta():
@@ -86,7 +60,7 @@ extensions = [
     'sphinx.ext.napoleon',
 
     # Used to include .md files:
-    'm2r',
+    'm2r2',
 
     # Used to insert typehints into the final docs:
     'sphinx_autodoc_typehints',
