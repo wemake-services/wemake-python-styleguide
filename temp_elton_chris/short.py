b1196c521fe3177b6ebb83b8b0696b769d2b253c
eltonlin <eltonlin@umich.edu>
2020-11-18 14:09:31 -0500

    added temp folder for notes, unit test and test cases

---
+++ temp_elton_chris/short.py
@@ -0,0 +1,14 @@
+#
+# hi
+#
+
+#: Non-empty doc comment
+
+#: 
+
+# c
+inta = 4
+intb = 7
+int_sum = inta + intb
+
+print(int_sum)  # noqa: WPS421
---
+++ tests/test_visitors/test_tokenize/test_comments/test_empty_comment.py
@@ -0,0 +1,113 @@
+import pytest
+
+from wemake_python_styleguide.violations.best_practices import (
+    WrongDocCommentViolation,
+)
+from wemake_python_styleguide.visitors.tokenize.comments import (
+    WrongCommentVisitor,
+)
+
+# Replace the violation and visitor later after we implement them
+
+# Be aware of code coverage
+# what about alerting in the right line? since we only alert the first line of a block
+
+
+single_empty_wrapped = """
+{0}
+#
+{0}
+"""
+
+multi_empty_wrapped = """
+{0}
+#
+#
+{0}
+"""
+
+# empty_line = ""
+# empty_comment = "#"
+
+
+single_empty_beginging = """
+#
+{0}
+"""
+
+single_empty_end = """
+{0}
+#
+"""
+
+multi_empty_begining = """
+#
+#
+{0}
+"""
+
+multi_empty_end = """
+{0}
+#
+#
+"""
+
+non_empty_comment = "# Non empty text"
+code_statement = "my_var = 1"
+
+@pytest.mark.parametrize('pattern', [
+    single_empty_wrapped,
+    multi_empty_wrapped
+])
+@pytest.mark.parametrize('comment', [
+    non_empty_comment
+])
+def test_correct_doc_comment(
+    parse_tokens,
+    assert_errors,
+    default_options,
+    pattern,
+    comment,
+):
+    """Ensures that correct comments do not raise a warning."""
+    # file_tokens = parse_tokens(pattern.format(comment))
+
+    print(pattern.format(comment))
+
+    # visitor = WrongCommentVisitor(default_options, file_tokens=file_tokens)
+    # visitor.run()
+    # print(visitor)
+    assert(False)
+
+    # assert_errors(visitor, [])
+
+
+@pytest.mark.parametrize('pattern', [
+    single_empty_beginging,
+    single_empty_end,
+    multi_empty_begining,
+    multi_empty_end
+
+])
+@pytest.mark.parametrize('code_or_comment', [
+    non_empty_comment,
+    code_statement
+])
+def test_incorrect_doc_comment(
+    parse_tokens,
+    assert_errors,
+    default_options,
+    pattern,
+    code_or_comment,
+):
+    """Ensures that incorrect empty comments raise a warning."""
+    # file_tokens = parse_tokens(pattern.format(code_or_comment))
+
+    print(pattern.format(code_or_comment))
+
+    # visitor = WrongCommentVisitor(default_options, file_tokens=file_tokens)
+    # visitor.run()
+    # print(visitor)
+    assert(False)
+
+    # assert_errors(visitor, [WrongDocCommentViolation])
