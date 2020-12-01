f96dcd63da10b81091405003c86425e1fcc64764
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2020-12-01 09:33:16 +0100

    remove quoting from GitHub actions entrypoint
    Remove quoting of `$INPUT_PATH`, because it was generating wrong `flake8` invocations, that causes following error:
    
    > .github/workflows/wps.yml wemake_python_styleguide/types.py wemake_python_styleguide/visitors/tokenize/comments.py :0:1: E902 FileNotFoundError: [Errno 2] No such file or directory: '.github/workflows/wps.yml wemake_python_styleguide/types.py wemake_python_styleguide/visitors/tokenize/comments.py '

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,7 +10,7 @@ echo
 
 # Runs flake8, possibly with reviewdog:
 if [ "$INPUT_REPORTER" == 'terminal' ]; then
-  output=$(flake8 "$INPUT_PATH")
+  output=$(flake8 $INPUT_PATH)
   status="$?"
 elif [ "$INPUT_REPORTER" == 'github-pr-review' ] ||
      [ "$INPUT_REPORTER" == 'github-pr-check' ]; then
@@ -18,7 +18,7 @@ elif [ "$INPUT_REPORTER" == 'github-pr-review' ] ||
   export REVIEWDOG_GITHUB_API_TOKEN="$GITHUB_TOKEN"
 
   # Running special version of `flake8` to mathc the `reviewdog` format:
-  output=$(flake8 "$INPUT_PATH" --append-config='/action-config.cfg')
+  output=$(flake8 $INPUT_PATH --append-config='/action-config.cfg')
   echo "$output" | reviewdog -f=pep8 -reporter="$INPUT_REPORTER" -level=error
   # `reviewdog` does not fail with any status code, so we have to get dirty:
   status=$(test "$output" = ''; echo $?)
