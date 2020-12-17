70f8aa2a4b885c022c9a4a0d8d8f6b4e90e16e82
Varsha <varsha.kumar@outlook.com>
2020-12-17 16:19:12 -0500

    Need to get rid of linter errors

--- tests/test_visitors/test_tokenize/test_statements/test_inconsistent_comprehension.py
+++ tests/test_visitors/test_tokenize/test_statements/test_inconsistent_comprehension.py
@@ -15,10 +15,6 @@ correct_empty_list = """
 a = []
 """
 
-correct_full_list = """
-a = [1,2,3]
-"""
-
 correct_one_line_comprehension = """
 [some(number) for number in numbers]
 """
@@ -31,14 +27,69 @@ correct_well_spaced_comprehension = """
 ]
 """
 
+nested_comprehension = """
+def get_all_args(call: ast.Call) -> Sequence[ast.AST]:
+return [
+               *call.args,
+               *[kw.value for kw in call.keywords],
+           ]
+"""
+
 
 @pytest.mark.parametrize('code', [
     correct_empty_list,
-    correct_full_list,
     correct_one_line_comprehension,
     correct_well_spaced_comprehension,
+    nested_comprehension,
 ])
-def test_correct_comprehension_consistency(
+def test_correct_list_comprehension(
+        parse_tokens,
+        assert_errors,
+        default_options,
+        code,
+):
+    """Ensures that correct consistency does not raise a warning."""
+    file_tokens = parse_tokens(code)
+
+    visitor = InconsistentComprehensionVisitor(default_options, file_tokens)
+    visitor.run()
+
+    assert_errors(visitor, [])
+
+
+# Set comprehensions
+
+correct_empty_set = """
+{}
+"""
+
+correct_one_line_comprehension = """
+{some(number) for number in numbers}
+"""
+
+correct_well_spaced_comprehension = """
+{
+    some(number)
+    for number in matrix
+    if number > 0
+}
+"""
+
+# The below test is inspired by:
+# https://python-reference.readthedocs.io/en/
+#       latest/docs/comprehensions/set_comprehension.html
+nested_comprehension = """
+{s for s in [1, 2, 3, 4]}
+"""
+
+
+@pytest.mark.parametrize('code', [
+    correct_empty_set,
+    correct_one_line_comprehension,
+    correct_well_spaced_comprehension,
+    nested_comprehension,
+])
+def test_correct_set_comprehension(
     parse_tokens,
     assert_errors,
     default_options,
@@ -52,21 +103,80 @@ def test_correct_comprehension_consistency(
 
     assert_errors(visitor, [])
 
+# Should raise flag
 
-# Should raise fag
-
-wrong_because_almost_one_line = """
+# List comprehensions
+wrong_almost_one_line = """
 [
     some(number) for number in numbers
     if number > 0
 ]
 """
 
+wrong_two_fors = """
+[
+    some(number)
+    for numbers in matrix for number in numbers
+]
+"""
+
+wrong_for_and_if = """
+[
+    some(number)
+    for number in matrix if number > 0
+]
+"""
+
 
 @pytest.mark.parametrize('code', [
-    wrong_because_almost_one_line,
+    wrong_almost_one_line,
+    wrong_two_fors,
+    wrong_for_and_if,
 ])
-def test_wrong_multiline_string_use(
+def test_wrong_list_comprehension(
+    parse_tokens,
+    assert_errors,
+    default_options,
+    code,
+):
+    """Ensures that wrong comprehension consistencies raise a warning."""
+    file_tokens = parse_tokens(code)
+    
+    visitor = InconsistentComprehensionVisitor(default_options, file_tokens)
+    visitor.run()
+    
+    assert_errors(visitor, [InconsistentComprehensionViolation])
+
+# Set comprehensions
+
+wrong_almost_one_line = """
+{
+    some(number) for number in numbers
+    if number > 0
+}
+"""
+
+wrong_two_fors = """
+{
+    some(number)
+    for numbers in matrix for number in numbers
+}
+"""
+
+wrong_for_and_if_set = """
+{
+    some(number)
+    for number in matrix if number > 0
+}
+"""
+
+
+@pytest.mark.parametrize('code', [
+    wrong_almost_one_line,
+    wrong_two_fors,
+    wrong_for_and_if,
+])
+def test_wrong_set_comprehension_consistency(
     parse_tokens,
     assert_errors,
     default_options,
