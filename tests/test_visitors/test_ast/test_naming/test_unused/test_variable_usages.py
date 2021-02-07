7246b0c47ef29e4b33243ed5f57fe45b54e27e7e
sobolevn <mail@sobolevn.me>
2021-02-07 15:42:49 +0300

    Fixes CI

--- tests/test_visitors/test_ast/test_naming/test_unused/test_variable_usages.py
+++ tests/test_visitors/test_ast/test_naming/test_unused/test_variable_usages.py
@@ -3,9 +3,11 @@ import pytest
 from wemake_python_styleguide.violations.naming import (
     UnusedVariableIsUsedViolation,
 )
+from wemake_python_styleguide.visitors.ast.naming.validation import (
+    WrongNameVisitor,
+)
 from wemake_python_styleguide.visitors.ast.naming.variables import (
     UnusedVariableUsageVisitor,
-    WrongNameVisitor,
 )
 
 annotation = 'some_var: {0}'
