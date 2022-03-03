cfafdb741936f785e4a5cf64e50770164d44a384
marcel <31695+dreamflasher@users.noreply.github.com>
2022-03-03 14:00:53 +0000

    lint

--- wemake_python_styleguide/logic/complexity/overuses.py
+++ wemake_python_styleguide/logic/complexity/overuses.py
@@ -27,7 +27,7 @@ def is_super_call(node: ast.AST) -> bool:
     And it is fine.
     """
     if isinstance(node, ast.Call) and isinstance(node.func, ast.Name):
-        return node.func.id == "super"
+        return node.func.id == 'super'
     return False
 
 
@@ -64,9 +64,9 @@ def is_self(node: ast.AST) -> bool:
         self_node = node
 
     return bool(
-        self_node
-        and isinstance(self_node.value, ast.Name)
-        and self_node.value.id in SPECIAL_ARGUMENT_NAMES_WHITELIST,
+        self_node and
+        isinstance(self_node.value, ast.Name) and
+        self_node.value.id in SPECIAL_ARGUMENT_NAMES_WHITELIST,
     )
 
 
@@ -84,8 +84,9 @@ def is_primitive(node: ast.AST) -> bool:
     if isinstance(node, (ast.Tuple, ast.List)):
         return not node.elts  # we do allow `[]` and `()`
     elif isinstance(node, ast.Set):
-        return len(node.elts) == 1 and isinstance(  # we do allow `{*set_items}`
-            node.elts[0], ast.Starred
+        return (  # we do allow `{*set_items}`
+            len(node.elts) == 1 and
+            isinstance(node.elts[0], ast.Starred)
         )
     elif isinstance(node, ast.Dict):  # we do allow `{}` and `{**values}`
         return not list(filter(None, node.keys))
@@ -101,14 +102,11 @@ def is_unary_minus(node: ast.AST) -> bool:
     We use this predicate to allow values
     like ``-some_value`` to be overused.
 
-    Although negative constants like ``-2``
+    Although negative constants like ``-1``
     should raise violation to force naming them.
     """
-    return (
-        isinstance(node, ast.UnaryOp)
-        and isinstance(node.op, ast.USub)
-        and (
-            not isinstance(node.operand, (Constant, ast.Num))
-            or node.operand.n == -1
-        )
+    return (isinstance(node, ast.UnaryOp) and
+            isinstance(node.op, ast.USub) and
+            (not isinstance(node.operand, (Constant, ast.Num)) or
+            node.operand.n == -1)
     )
