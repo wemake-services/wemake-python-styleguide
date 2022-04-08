49a6bbbc52aade4e05df7fce240447aff4179131
Nikita Sobolev <mail@sobolevn.me>
2022-04-08 14:07:35 +0300

    Fix click security issue (#2393)
    * Fix click security issue
    
    * Fix CI
    
    * Fix CI

--- scripts/check_generic_visit.py
+++ scripts/check_generic_visit.py
@@ -1,15 +1,17 @@
 import sys
+from typing import NoReturn
 
 import astpath
 from pygments import highlight
 from pygments.formatters.terminal256 import Terminal256Formatter
 from pygments.lexers.python import PythonLexer
+from typing_extensions import Final
 
-FAIL_CODE = 255
+FAIL_CODE: Final = 255
 
-OK_CODE = 0
+OK_CODE: Final = 0
 
-PATTERN = """
+PATTERN: Final = """
 //ClassDef[contains(bases, Name[@id='BaseNodeVisitor'])]/body
 /FunctionDef[re:match('visit_.*', @name)
 and not(child::body/Expr[last()]/value/Call/func
@@ -17,30 +19,37 @@ and not(child::body/Expr[last()]/value/Call/func
 """
 
 # This is needed to stop linter from spewing WPS421 errors.
-my_print = print
+report = print
 
 
-if __name__ == '__main__':
+def main() -> NoReturn:
+    """Check for ``self.generic_visit()`` in all visit methods."""
     if len(sys.argv) == 1:
-        my_print('Please provide path to search in!')
+        report('Please provide path to search in!')
 
     matches = astpath.search(sys.argv[1], PATTERN, print_matches=False)
 
     if not len(matches):
         exit(OK_CODE)  # noqa: WPS421
 
-    my_print()
-    my_print('"self.generic_visit(node)" should be last statement here:')
+    report()
+    report('"self.generic_visit(node)" should be last statement here:')
 
     for fn, line in matches:
         with open(fn, 'r') as fp:
-            source = fp.read()
-            lines = source.splitlines()
-            highlighted = highlight(
-                lines[line - 1],
-                PythonLexer(),
-                Terminal256Formatter(),
+            lines = fp.read().splitlines()
+            report('\t{0}:{1}\n\t{2}'.format(
+                fn,
+                line,
+                highlight(
+                    lines[line - 1],
+                    PythonLexer(),
+                    Terminal256Formatter(),
+                )),
             )
-            my_print('\t{0}:{1}\n\t{2}'.format(fn, line, highlighted))
 
     exit(FAIL_CODE)  # noqa: WPS421
+
+
+if __name__ == '__main__':
+    main()
