cf783795c6cd2e732d62b5854be21d20f1e006c4
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-09-08 21:37:35 +0300

    fix baseline for formatting violation

--- wemake_python_styleguide/visitors/ast/function_empty_lines.py
+++ wemake_python_styleguide/visitors/ast/function_empty_lines.py
@@ -1,4 +1,5 @@
 import ast
+import math
 
 from typing_extensions import final
 
@@ -32,25 +33,20 @@ class WrongEmptyLinesCountVisitor(base.BaseNodeVisitor):
             empty_lines_count = len(
                 lines_range - set(line_numbers_with_expressions),
             )
+            available_empty_lines = self._available_empty_lines(
+                len(line_numbers_with_expressions),
+            )
             if not empty_lines_count:
                 continue
-            if not self._proportion_allow(empty_lines_count, len(lines_range)):
+            if empty_lines_count >= available_empty_lines:
                 self.add_violation(
                     best_practices.WrongEmptyLinesCountVisitorViolation(
                         node,
                         text=str(empty_lines_count),
-                        baseline=0,
+                        baseline=available_empty_lines,
                     ),
                 )
 
-    def _proportion_allow(
-        self,
-        empty_lines_count: int,
-        function_body_len: int,
-    ) -> bool:
-        empty_lines_proportion = empty_lines_count / function_body_len
+    def _available_empty_lines(self, lines_with_expressions_count: int) -> int:
         option = self.options.available_expressions_for_one_empty_line
-        if option == 0 and empty_lines_count:
-            return False
-        available_values = 1 / option
-        return empty_lines_proportion > available_values
+        return math.floor(lines_with_expressions_count / option)
