2bbf06b442cee86925238ea0b453402f524af600
sobolevn <mail@sobolevn.me>
2024-12-18 17:28:52 +0300

    Fix coverage

--- wemake_python_styleguide/logic/scopes/predicates.py
+++ wemake_python_styleguide/logic/scopes/predicates.py
@@ -88,9 +88,9 @@ def is_import_in_try(node: ast.AST) -> bool:
         return False
     # We don't use `ast.TryStar` here because it is not a common
     # pattern to have imports in `try/except*` blocks.
-    if isinstance(parent, ast.ExceptHandler) and isinstance(
-        get_parent(parent),
-        TryStar,
+    if (  # pragma: <3.11 no cover
+        isinstance(parent, ast.ExceptHandler)
+        and isinstance(get_parent(parent), TryStar)
     ):
         return False
     # We still require imports to be top-level:
