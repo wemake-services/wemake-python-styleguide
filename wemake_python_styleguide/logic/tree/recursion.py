491042ca5d449cab4049fa79d645e384d1a3b38a
sobolevn <mail@sobolevn.me>
2024-12-23 22:04:19 +0300

    Enable `ICN` in `ruff`

--- wemake_python_styleguide/logic/tree/recursion.py
+++ wemake_python_styleguide/logic/tree/recursion.py
@@ -1,28 +1,28 @@
-from ast import AST, Attribute, Call, ClassDef, walk
+import ast
 
 from wemake_python_styleguide.logic.nodes import get_context
 from wemake_python_styleguide.logic.tree.functions import given_function_called
 from wemake_python_styleguide.types import AnyFunctionDef
 
 
-def _is_self_call(func: AnyFunctionDef, node: AST) -> bool:
+def _is_self_call(func: AnyFunctionDef, node: ast.AST) -> bool:
     return (
-        isinstance(node, Call)
-        and isinstance(node.func, Attribute)
+        isinstance(node, ast.Call)
+        and isinstance(node.func, ast.Attribute)
         and bool(given_function_called(node, {f'self.{func.name}'}))
     )
 
 
 def _check_method_recursion(func: AnyFunctionDef) -> bool:
-    return bool([node for node in walk(func) if _is_self_call(func, node)])
+    return bool([node for node in ast.walk(func) if _is_self_call(func, node)])
 
 
 def _check_function_recursion(func: AnyFunctionDef) -> bool:
     return bool(
         [
             node
-            for node in walk(func)
-            if isinstance(node, Call)
+            for node in ast.walk(func)
+            if isinstance(node, ast.Call)
             and given_function_called(node, {func.name})
         ],
     )
@@ -34,6 +34,6 @@ def has_recursive_calls(func: AnyFunctionDef) -> bool:
 
     Does not work for methods.
     """
-    if isinstance(get_context(func), ClassDef):
+    if isinstance(get_context(func), ast.ClassDef):
         return _check_method_recursion(func)
     return _check_function_recursion(func)
