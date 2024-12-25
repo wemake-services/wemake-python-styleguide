82de53b4d2a5b62d194c04e3f626b29b44dd0d95
sobolevn <mail@sobolevn.me>
2024-12-25 20:16:32 +0300

    Fix CI

--- wemake_python_styleguide/visitors/ast/functions.py
+++ wemake_python_styleguide/visitors/ast/functions.py
@@ -1,9 +1,7 @@
 import ast
 from collections.abc import Mapping
 from contextlib import suppress
-from typing import ClassVar, TypeAlias
-
-from typing_extensions import final
+from typing import ClassVar, TypeAlias, final
 
 from wemake_python_styleguide.compat.aliases import (
     ForNodes,
@@ -12,7 +10,7 @@ from wemake_python_styleguide.compat.aliases import (
 from wemake_python_styleguide.constants import (
     FUNCTIONS_BLACKLIST,
 )
-from wemake_python_styleguide.logic import nodes, source, walk
+from wemake_python_styleguide.logic import nodes, walk
 from wemake_python_styleguide.logic.arguments import function_args
 from wemake_python_styleguide.logic.naming import access
 from wemake_python_styleguide.logic.tree import (
@@ -49,9 +47,6 @@ from wemake_python_styleguide.visitors import base, decorators
 #: Things we treat as local variables.
 _LocalVariable: TypeAlias = ast.Name | ast.ExceptHandler
 
-#: Function definitions with name and arity:
-_Defs: TypeAlias = Mapping[str, int]
-
 
 @final
 class WrongFunctionCallVisitor(base.BaseNodeVisitor):
@@ -60,20 +55,6 @@ class WrongFunctionCallVisitor(base.BaseNodeVisitor):
 
     All these functions are defined in ``FUNCTIONS_BLACKLIST``.
     """
-
-    _functions: ClassVar[_Defs] = {
-        'getattr': 3,
-        'setattr': 3,
-    }
-
-    _postfixes: ClassVar[_Defs] = {
-        # dict methods:
-        '.get': 2,
-        '.pop': 2,
-        '.setdefault': 2,
-        # list methods:
-        '.insert': 2,
-    }
 
     def visit_Call(self, node: ast.Call) -> None:
         """Used to find ``FUNCTIONS_BLACKLIST`` calls."""
@@ -121,21 +102,6 @@ class WrongFunctionCallVisitor(base.BaseNodeVisitor):
             self.add_violation(
                 oop.WrongSuperCallViolation(node, text='remove arguments'),
             )
-
-    def _is_call_ignored(self, node: ast.Call) -> bool:
-        call = source.node_to_string(node.func)
-        func_called = functions.given_function_called(
-            node,
-            self._functions.keys(),
-        )
-
-        return bool(
-            func_called and len(node.args) == self._functions[func_called],
-        ) or any(
-            call.endswith(post)
-            for post in self._postfixes
-            if len(node.args) == self._postfixes[post]
-        )
 
 
 @final
