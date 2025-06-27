d6f5b9dcaefbca4eea314d0c71aa303d940a656e
Alexeev Bronislav <alexeev.dev@mail.ru>
2025-06-27 21:28:06 +0700

    add visitor to presets

--- wemake_python_styleguide/presets/topics/complexity.py
+++ wemake_python_styleguide/presets/topics/complexity.py
@@ -13,6 +13,7 @@ from wemake_python_styleguide.visitors.ast.complexity import (  # noqa: WPS235
     offset,
     overuses,
     pm,
+    complex_continue
 )
 
 #: Used to store all complexity related visitors to be later passed to checker:
@@ -39,4 +40,5 @@ PRESET: Final = (
     annotations.AnnotationComplexityVisitor,
     pm.MatchSubjectsVisitor,
     pm.MatchCasesVisitor,
+    complex_continue.ComplexFinallyBlocksVisitor
 )
