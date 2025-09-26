c51abf343b093a7c7192877a2821e0087457f2da
sobolevn <mail@sobolevn.me>
2025-09-26 14:09:33 +0300

    Make `furo` more content-full (#3534)

--- docs/conf.py
+++ docs/conf.py
@@ -109,22 +109,11 @@ add_module_names = False
 # a list of builtin themes.
 html_theme = 'furo'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {}
+html_theme_options = {
+    'source_repository': str(pkg_meta['urls']['Repository']),
+    'source_branch': 'master',
+    'source_directory': 'docs/',
+}
 
 # -- Extension configuration -------------------------------------------------
 
